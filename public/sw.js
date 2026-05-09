3f6755daa79722844b022db7953e3d78c381792a
Ralph Wijnands <ralph.wijnands1988@gmail.com>
2026-05-10 00:47:11 +0200

    Fix iPhone layout and PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'piano-studio-v2';
+const CACHE_NAME = 'piano-studio-v3';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
